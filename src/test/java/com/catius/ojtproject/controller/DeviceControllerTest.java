e622b5d3e02cb49906b06b4c98762fccfe41e9f2
ys313 <ys3135@gmail.com>
2021-08-30 18:46:14 +0900

    CRUD 서비스 테스트 까지 컨트롤러 해야함

--- src/test/java/com/catius/ojtproject/controller/DeviceControllerTest.java
+++ src/test/java/com/catius/ojtproject/controller/DeviceControllerTest.java
@@ -1,12 +1,18 @@
 package com.catius.ojtproject.controller;
 
 import com.catius.ojtproject.service.DeviceServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
 import java.nio.charset.StandardCharsets;
 
@@ -19,10 +25,24 @@ public class DeviceControllerTest {
     @MockBean
     private DeviceServiceImpl deviceServiceImpl;
 
+    @InjectMocks
+    private DeviceController deviceController;
+
     protected MediaType contentType =
             new MediaType(MediaType.APPLICATION_JSON.getSubtype(),
                     MediaType.APPLICATION_JSON.getSubtype(),
                     StandardCharsets.UTF_8);
+
+    @Before
+    public void setup() {
+        JacksonTester.initFields(this, new ObjectMapper());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(deviceController)
+                .setControllerAdvice(new ExceptionHandlerExceptionResolver())
+                .build();
+
+
+    }
 
     @Test
     public void createDevice() {
